627529c1b1d0819c2f64608a7c5c30c23d41be36
Kaitlyn <91761833+keezo11203@users.noreply.github.com>
2022-12-05 16:36:14 -0500

    Update fib.asm

--- fib.asm
+++ fib.asm
@@ -1,17 +1,20 @@
 #where memory and data is kept
-
 .data
     prompt: .asciiz "\nEnter a positive number greater than or equal to 25: "
     error: .asciiz "\nThat is not a valid number. Try again!"
     valid: .asciiz "\n This number is valid."
     final: .asciiz "The fibonacci sequence is:\n"
-
+    
 #where the instructions are
 
 .text
 .global main
     main:
     
-    
     while:
-        li $v0, 4
+        li $v0, 4                           # load system call 4 into register v0
+        la $a0, prompt                      # load prompt address in register a0
+        syscall                             # perform system call
+        
+        li $v0, s
+        syscall

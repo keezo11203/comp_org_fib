309f1392adebfc5931c5526a33b54999c82b569a
Kaitlyn <91761833+keezo11203@users.noreply.github.com>
2022-12-05 16:14:38 -0500

    Update fib.asm

--- fib.asm
+++ fib.asm
@@ -1,6 +1,17 @@
 #where memory and data is kept
+
 .data
     prompt: .asciiz "\nEnter a positive number greater than or equal to 25: "
     error: .asciiz "\nThat is not a valid number. Try again!"
     valid: .asciiz "\n This number is valid."
     final: .asciiz "The fibonacci sequence is:\n"
+
+#where the instructions are
+
+.text
+.global main
+    main:
+    
+    
+    while:
+        li $v0, 4
